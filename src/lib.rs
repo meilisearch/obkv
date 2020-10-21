0576a5ae1af9db6cd741abccd934cb903574c46f
Clément Renault <clement@meilisearch.com>
2020-10-21 11:47:17 +0200

    Introduce the KvWriter and KvReader types

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+#[cfg(test)]
+#[macro_use] extern crate quickcheck;
+
+mod varint;
+
+use std::io::{self, Error, ErrorKind::Other};
+use std::convert::TryInto;
+
+use self::varint::{varint_encode32, varint_decode32};
+
+pub struct KvWriter<W> {
+    last_key: Option<u8>,
+    writer: W,
+}
+
+impl<W> KvWriter<W> {
+    pub fn new(writer: W) -> KvWriter<W> {
+        KvWriter { last_key: None, writer }
+    }
+}
+
+impl KvWriter<Vec<u8>> {
+    pub fn memory() -> KvWriter<Vec<u8>> {
+        KvWriter { last_key: None, writer: Vec::new() }
+    }
+}
+
+impl<W: io::Write> KvWriter<W> {
+    pub fn insert(&mut self, key: u8, value: impl AsRef<[u8]>) -> io::Result<()> {
+        if self.last_key.map_or(false, |last| key <= last) {
+            return Err(Error::new(Other, "keys must be inserted in order and only one time"));
+        }
+
+        let val = value.as_ref();
+        let val_len = match val.len().try_into() {
+            Ok(len) => len,
+            Err(_) => return Err(Error::new(Other, "value length is bigger than u32 MAX")),
+        };
+
+        let mut buffer = [0; 5];
+        let len_bytes = varint_encode32(&mut buffer, val_len);
+
+        self.writer.write_all(&[key])?;
+        self.writer.write_all(len_bytes)?;
+        self.writer.write_all(val)?;
+
+        self.last_key = Some(key);
+
+        Ok(())
+    }
+
+    pub fn into_inner(mut self) -> io::Result<W> {
+        self.writer.flush()?;
+        Ok(self.writer)
+    }
+
+    pub fn finish(self) -> io::Result<()> {
+        self.into_inner().map(drop)
+    }
+}
+
+pub struct KvReader<'a> {
+    bytes: &'a [u8],
+}
+
+impl<'a> KvReader<'a> {
+    pub fn new(bytes: &[u8]) -> KvReader {
+        KvReader { bytes }
+    }
+
+    pub fn get(&self, requested_key: u8) -> Option<&'a [u8]> {
+        let mut offset = 0;
+
+        loop {
+            let key = *self.bytes.get(offset)?;
+            offset += 1;
+
+            let val_len = {
+                let mut val_len = 0;
+                let bytes = self.bytes.get(offset..)?;
+                offset += varint_decode32(bytes, &mut val_len)?;
+                val_len as usize
+            };
+
+            let val = self.bytes.get(offset..offset + val_len)?;
+            offset += val_len;
+
+            if key == requested_key {
+                return Some(val);
+            }
+
+            if key > requested_key {
+                return None;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn easy() {
+        let mut writer = KvWriter::memory();
+        writer.insert(0, b"hello").unwrap();
+        writer.insert(1, b"blue").unwrap();
+        writer.insert(255, b"world").unwrap();
+        let obkv = writer.into_inner().unwrap();
+
+        let reader = KvReader::new(&obkv);
+        assert_eq!(reader.get(0), Some(&b"hello"[..]));
+        assert_eq!(reader.get(1), Some(&b"blue"[..]));
+        assert_eq!(reader.get(10), None);
+        assert_eq!(reader.get(255), Some(&b"world"[..]));
+    }
+}
---
+++ src/varint.rs
@@ -0,0 +1,77 @@
+fn varint_length_packed(data: &[u8]) -> Option<u32> {
+    let mut i = 0;
+    for _ in 0..data.len() {
+        if (*data.get(i)? & 0x80) == 0 {
+            break;
+        }
+        i += 1;
+    }
+    if i == data.len() { Some(0) } else { Some(i as u32 + 1) }
+}
+
+#[must_use]
+pub fn varint_encode32(bytes: &mut [u8], value: u32) -> &[u8] {
+    let b = 128;
+
+    if value < (1 << 7) {
+        bytes[0] = value as u8;
+        &bytes[..1]
+    } else if value < (1 << 14) {
+        bytes[0] = (value | b) as u8;
+        bytes[1] = (value >> 7) as u8;
+        &bytes[..2]
+    } else if value < (1 << 21) {
+        bytes[0] = (value | b) as u8;
+        bytes[1] = ((value >> 7) | b) as u8;
+        bytes[2] = (value >> 14) as u8;
+        &bytes[..3]
+    } else if value < (1 << 28) {
+        bytes[0] = (value | b) as u8;
+        bytes[1] = ((value >> 7) | b) as u8;
+        bytes[2] = ((value >> 14) | b) as u8;
+        bytes[3] = (value >> 21) as u8;
+        &bytes[..4]
+    } else {
+        bytes[0] = (value | b) as u8;
+        bytes[1] = ((value >> 7) | b) as u8;
+        bytes[2] = ((value >> 14) | b) as u8;
+        bytes[3] = ((value >> 21) | b) as u8;
+        bytes[4] = (value >> 28) as u8;
+        &bytes[..5]
+    }
+}
+
+pub fn varint_decode32(data: &[u8], value: &mut u32) -> Option<usize> {
+    let len = varint_length_packed(data.get(..data.len().min(5))?)?;
+    let mut val = (*data.get(0)? & 0x7f) as u32;
+    if len > 1 {
+        val |= ((*data.get(1)? & 0x7f) as u32) << 7;
+        if len > 2 {
+            val |= ((*data.get(2)? & 0x7f) as u32) << 14;
+            if len > 3 {
+                val |= ((*data.get(3)? & 0x7f) as u32) << 21;
+                if len > 4 {
+                    val |= (*data.get(4)? as u32) << 28;
+                }
+            }
+        }
+    }
+    *value = val;
+    Some(len as usize)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    quickcheck! {
+        fn qc_codec_u32(num: u32) -> bool {
+            let mut buf = [0; 10];
+            let mut val = 0;
+            let buf = varint_encode32(&mut buf, num);
+            varint_decode32(buf, &mut val).unwrap();
+
+            num == val
+        }
+    }
+}
